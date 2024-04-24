c979888af9d559158b0a241769a66aec3f1652dc
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-04-24 14:15:24 -0300

    Add project files.

---
+++ matrizQuadradaOperacao/Program.cs
@@ -0,0 +1,43 @@
+﻿int tamanho = 3;
+int qtdlinhas = tamanho;
+int qtdcolunas = tamanho;
+int[,] matriz = new int[qtdlinhas, qtdcolunas];
+int [] resultadoLinha = new int[tamanho];
+
+for (int linha = 0; linha < qtdlinhas; linha++)
+{
+    for (int coluna = 0; coluna < qtdcolunas; coluna++)
+    {
+        matriz[linha, coluna] = new Random().Next(1, 10);
+    }
+}
+
+for (int linha = 0; linha < qtdlinhas; linha++)
+{
+    Console.WriteLine();
+    for (int coluna = 0; coluna < qtdcolunas; coluna++)
+    {
+        Console.Write(matriz[linha, coluna] + "| ");
+    }
+}
+
+//Soma Linhas
+
+for (int linha = 0; linha < qtdlinhas; linha++)
+{
+    Console.WriteLine();
+    for (int coluna = 0; coluna < qtdcolunas; coluna++)
+    {
+
+    resultadoLinha [linha] = matriz[linha,coluna] + matriz[linha, coluna];
+
+    }  
+}
+
+Console.WriteLine();
+for (int i = 0; i < tamanho; i++)
+
+{
+    Console.WriteLine(resultadoLinha[i] + i+1);
+}
+
